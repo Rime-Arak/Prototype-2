affa1a4a22d1091b9a99084d8f62ced71967fe3d
Rime-Arak <emirkaramehmetoglu@gmail.com>
2026-03-23 15:44:57 +0300

    Initial commit

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20.0f;
+    private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 2;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX+1), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation); 
+    }
+}
